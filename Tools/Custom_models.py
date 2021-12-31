5c08679e9fb27f599ea87f17bcdd189010aad068
Cyril Vallez <cyril.vallez@epfl.ch>
2021-12-31 17:43:44 +0100

    Big clean up

--- Tools/Custom_models.py
+++ Tools/Custom_models.py
@@ -13,36 +13,45 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv1D
 from tensorflow.keras import regularizers
 
+#---------------------------------------------- CUSTOM SCHEDULERS ------------------------------------------------------------
+
+def step_scheduler(step, rate):
+    """ Step learning rate scheduler. Every step epochs, it decreases the learning rate by rate.
+        It returns a function, to be used in a Callback during training """
+    def scheduler(epoch, lr):
+        if (epoch % step == 0 and epoch>0):
+            return lr*rate
+        else:
+            return lr
+        
+    return scheduler
+
+
+def exponential_scheduler(start, rate):
+    """ exponential learning rate scheduler. after start epochs, it decreases the learning rate by rate every epoch.
+        It returns a function, to be used in a Callback during training """
+    def scheduler(epoch, lr):
+        if (epoch < start):
+            return lr
+        else:
+            return lr*rate
+        
+    return scheduler
+
 #------------------------------------------------ CUSTOM INITIALIZATION -------------------------------------------------------
 
 def my_bias_initializer_1D(shape, dtype=None):
     """ Custom initialization for the non-trainable first layer in 1D"""
     N = shape[-1]
-    bias = -tf.linspace(0, 1, N)
+    bias = -tf.linspace(-1, 1, N)
     return tf.cast(bias, dtype=dtype)
     
-#------------------------------------------------------MODELS -------------------------------------------------------------------
+#------------------------------------------------------MODELS -------------------------------------------------------------------    
 
-class Model_1D_width(tf.keras.Model):
-    
-    def __init__(self, K1, K_output=1, name=None):
-        super(Model_1D_width, self).__init__(name=name)
-        self.dense1 = Dense(K1, activation='relu', name='dense1')
-        self.dense2 = Dense(K1, activation='relu', name='dense2')
-        self.dense3 = Dense(K_output, activation=None, name='dense3')
-
-    def call(self, inputs):
-        x = self.dense1(inputs)
-        x2 = self.dense2(x)
-        x3 = self.dense3(x2)
-        return x3
-
-#----------------------------------------------------------------------- 
-
-class Model_1D_depth(tf.keras.Model):
+class Model_depth(tf.keras.Model):
     
     def __init__(self, K1, K_output=1, depth=1, name=None):
-        super(Model_1D_depth, self).__init__(name=name)
+        super(Model_depth, self).__init__(name=name)
         self.sequence = tf.keras.Sequential(name='sequential')
         
         for i in range(depth):
@@ -54,8 +63,26 @@ class Model_1D_depth(tf.keras.Model):
         return x
 
 #----------------------------------------------------------------------- 
+
+class Model_depth_reg(tf.keras.Model):
+    
+    def __init__(self, K1, K_output=1, depth=1, reg=1e-2, name=None):
+        super(Model_depth_reg, self).__init__(name=name)
+        self.sequence = tf.keras.Sequential(name='sequential')
+        
+        for i in range(depth):
+            self.sequence.add(Dense(K1, activation='relu', name=f'dense{i+1}', 
+                                    kernel_regularizer=regularizers.l2(reg), bias_regularizer=regularizers.l2(reg)))
+        self.sequence.add(Dense(K_output, activation=None, name=f'dense{depth+1}',
+                               kernel_regularizer=regularizers.l2(reg), bias_regularizer=regularizers.l2(reg)))
+
+    def call(self, inputs):
+        x = self.sequence(inputs)
+        return x
+
+#----------------------------------------------------------------------- 
        
-class Model_1D_1_layer(tf.keras.Model):
+class Model_1_layer_trainable(tf.keras.Model):
     
     def __init__(self, K1, K_output=1, train_first_layer=True, name=None):
         super(Model_1D_1_layer, self).__init__(name=name)
@@ -73,7 +100,7 @@ class Model_1D_1_layer(tf.keras.Model):
                       
 #----------------------------------------------------------------------- 
 
-class Model_1D_conv(tf.keras.Model):
+class Model_conv(tf.keras.Model):
     
     def __init__(self, K1, kernel=3, K_output=1, train_first_layer=True, name=None):
         super(Model_1D_conv, self).__init__(name=name)
@@ -95,7 +122,7 @@ class Model_1D_conv(tf.keras.Model):
                       
 #----------------------------------------------------------------------- 
 
-class Model_1D_2_layers(tf.keras.Model):
+class Model_2_layers_reg(tf.keras.Model):
     
     def __init__(self, K1, K_output=1, reg_kernel=False, reg_biases=False, reg_value=0.01, name=None):
         super(Model_1D_2_layers, self).__init__(name=name)
@@ -119,7 +146,7 @@ class Model_1D_2_layers(tf.keras.Model):
 
 #----------------------------------------------------------------------- 
 
-class Model_1D_2_layers_trainable(tf.keras.Model):
+class Model_2_layers_trainable(tf.keras.Model):
     
     def __init__(self, K1, K_output=1, train_first_layer=True, name=None):
         super(Model_1D_2_layers_trainable, self).__init__(name=name)
@@ -139,19 +166,3 @@ class Model_1D_2_layers_trainable(tf.keras.Model):
     
 #-----------------------------------------------------------------------
                   
-  
-class Model_2D(tf.keras.Model):
-    
-    def __init__(self, K1, K2, K_output=1, name=None):
-        super(Model_2D, self).__init__(name=name)
-        self.dense1 = Dense(K1, activation='relu', name='dense1')
-        self.dense2 = Dense(K2, activation='relu', name='dense2')
-        self.dense3 = Dense(K_output, activation=None, name='dense3')
-
-    def call(self, inputs):
-        x = self.dense1(inputs)
-        x2 = self.dense2(x)
-        x3 = self.dense3(x2)
-        return x3
-                      
-#----------------------------------------------------------------------- 
--- Tools/Helpers.py
+++ Tools/Helpers.py
@@ -14,33 +14,36 @@ import tensorflow as tf
 
 #------------------------------------------ LOADING DATA ----------------------------------------------------------
 
-def Load1D(filename):
-    """ Load 1D data files """
+def Load(filename, D):
+    """ Load data files in D dimensions"""
     data = np.loadtxt(filename)
-    features = data[:,0]
-    labels = data[:,1]
+    features = data[:,0:D]
+    labels = data[:,D]
     
     return features, labels
 
-def split_data(features, labels, seed=6, ratio=0.9):
-    """ Split a dataset into training and testing according to the ratio given """
+def split_data_val(features, labels, seed=6, ratio_test=0.1, ratio_val=0.1):
+    """ Split a dataset into training, validation and testing according to the given ratio """
     np.random.seed(seed)
     N = len(features)
-    stop = round(ratio*N)
+    stop_test = round(ratio_test*N)
+    stop_val = round(ratio_val*N)
     perm = np.random.permutation(N)
-    features_training = features[perm[0:stop]]
-    features_testing = features[perm[stop:N]]
-    labels_training = labels[perm[0:stop]]
-    labels_testing = labels[perm[stop:N]]
+    features_test = features[perm[0:stop_test]]
+    labels_test = labels[perm[0:stop_test]]
+    features_val = features[perm[stop_test:stop_test+stop_val]]
+    labels_val = labels[perm[stop_test:stop_test+stop_val]]
+    features_train = features[perm[stop_test+stop_val:]]
+    labels_train = labels[perm[stop_test+stop_val:]]
     
-    return features_training, labels_training, features_testing, labels_testing
+    return features_train, labels_train, features_val, labels_val, features_test, labels_test
 
-def Load_and_split_1D(filename, seed=6, ratio=0.9):
-    """ Convenient shortcut for loading and spliting data in 1D """
+
+def Load_and_split_val(filename, D, seed=6, ratio_test=0.1, ratio_val=0.1):
     np.random.seed(seed)
-    features, labels = Load1D(filename)
+    features, labels = Load(filename, D)
     
-    return split_data(features, labels, seed, ratio)
+    return split_data_val(features, labels, seed, ratio_test, ratio_val)
     
     
 #------------------------------------------ FEM functions ----------------------------------------------------------
@@ -111,5 +114,61 @@ def basis_functions_conv_layer(x, layer1, layer2):
     x5 = layer2(x4)
     x6 = tf.squeeze(x5, axis=-1)
     basis_func = x6.numpy().T
+    
+    return basis_func
+
+
+def basis_functions_layer_2D(coordinates, layers):
+    """ Compute the basis functions created by an arbitrary layer on the coordinates in 2D
+    
+    layers : list of all the layers until the layer of interest
+    
+    For example if we want to visualize what happens in layer 3
+    --> layers = [layer1, layer2, layer3] 
+    
+    """
+    x = coordinates
+    for i in range(len(layers)):
+        x = layers[i](x)
+    basis_func = x.numpy().T
+    
+    N = int(np.sqrt(len(coordinates)))
+    basis_func_grid = []
+    
+    for i in range(len(basis_func)):
+        basis_func_grid.append(basis_func[i, :].reshape(N, N))
+    
+    return np.array(basis_func_grid)
+
+
+def normalize_basis_2D(basis_functions, tol=1e-8):
+    """ Normalize the basis functions as returned by basis_functions_layer_2D so that the functions are in
+        the range [0; 1]. Moreover, we only keep functions which are really activated (non-zero) up to tol, so that
+        the plots are more efficient.
+    """
+    basis_func = basis_functions.copy()   # So that we don't modify the original variable
+    useful_indices = []
+    for i in range(len(basis_func)):
+        if (np.linalg.norm(basis_func[i], 'fro') > tol):
+            basis_func[i] = (basis_func[i] - np.min(basis_func[i]))/(np.max(basis_func[i]) - np.min(basis_func[i]))
+            useful_indices.append(i)
+            
+    return basis_func[useful_indices]
+
+
+def basis_functions_radius(r, layers):
+    """ Compute the basis functions created by an arbitrary layer on the space interval denoted by the radius r, on the line
+        x1 = x2 = x3 = ... = xN  where N is the dimension of the input function
+    
+    layers : list of all the layers until the layer of interest
+    
+    For example if we want to visualize what happens in layer 3
+    --> layers = [layer1, layer2, layer3] 
+    
+    """
+    r2 = r.copy()
+    for i in range(len(layers)):
+        r2 = layers[i](r2)
+    basis_func = r2.numpy().T
     
     return basis_func
